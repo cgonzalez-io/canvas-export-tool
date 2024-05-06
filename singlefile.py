3feb20b9abbb32d400fce0e5a5718bac95898433
cgonzalez-io <c.gonzalez@encetech.org>
2024-05-06 07:40:15 -0600

    Add cookie conversion script and update browser path in singlefile.py
    This commit introduces a new cookie conversion script that converts cookies into a format readable by tools like curl or youtube-dl. This script handles cookies from Chrome's Application Storage. Additionally, the default browser executable path in the singlefile.py script has been updated for better compatibility. Miscellaneous changes were made to the .gitignore file and the package-lock.json has been updated with new dependencies.

--- singlefile.py
+++ singlefile.py
@@ -1,27 +1,29 @@
 from subprocess import run
 
 SINGLEFILE_BINARY_PATH = "./node_modules/single-file/cli/single-file"
-CHROME_PATH = "C:/Program Files/Google\ Chrome/Application/chrome.exe" #Uncomment this and set your browser exe if it can't find yours.
+CHROME_PATH = "/opt/homebrew/bin/chromium"  #Uncomment this and set your browser exe if it can't find yours.
+
 
 def addQuotes(str):
-    return "\"" + str.strip("\"") + "\""
+    return "\""+str.strip("\"")+"\""
+
 
 def download_page(url, cookies_path, output_path, output_name_template = ""):
     args = [
         addQuotes(SINGLEFILE_BINARY_PATH),
-        #"--browser-executable-path=" + addQuotes(CHROME_PATH.strip("\"")), #Uncomment this and set your browser exe if it can't find yours.
-        "--browser-cookies-file=" + addQuotes(cookies_path),
-        "--output-directory=" + addQuotes(output_path),
+        "--browser-executable-path=" + addQuotes(CHROME_PATH.strip("\"")), #Uncomment this and set your browser exe if it can't find yours.
+        "--browser-cookies-file="+addQuotes(cookies_path),
+        "--output-directory="+addQuotes(output_path),
         addQuotes(url)
-        ]
+    ]
 
-    if(output_name_template != ""):
-        args.append("--filename-template=" + addQuotes(output_name_template))
+    if (output_name_template!=""):
+        args.append("--filename-template="+addQuotes(output_name_template))
 
     try:
-        run("node " + " ".join(args), shell=True)
+        run("node "+" ".join(args), shell = True)
     except Exception as e:
-        print("Was not able to save the URL " + url + " using singlefile. The reported error was " + e.strerror)
+        print("Was not able to save the URL "+url+" using singlefile. The reported error was "+e.strerror)
 
 #if __name__ == "__main__":
-    #download_page("https://www.google.com/", "", "./output/test", "test.html")
+#download_page("https://www.google.com/", "", "./output/test", "test.html")
